0b7e968fbf0a4678c581c1d9eff8e20ea2d1edbc
James Schneider <james.rice.schneider@gmail.com>
2019-03-16 10:51:06 -0400

    Add password reset sequence

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,3 +127,5 @@ STATICFILES_DIRS = (
 
 LOGIN_REDIRECT_URL = 'index'
 LOGOUT_REDIRECT_URL = 'index'
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
